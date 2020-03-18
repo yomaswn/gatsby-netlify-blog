062fa69e7da909370b36843840cd3d7ee0b32fb0
yoma sofwan <yoma@femaledaily.com>
2020-03-18 21:38:21 +0700

    Update card post

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -5,13 +5,19 @@ const PostLink = ({ post }) => {
   return (
     <article className='card '>
       <Link to={post.frontmatter.path}>
-        {!!post.frontmatter.thumbnail && (
+        {!!post.frontmatter.thumbnail ? (
           <img
             src={post.frontmatter.thumbnail}
             alt={post.frontmatter.title + '- Featured Shot'}
           />
+        ) : (
+          <img
+            src={'/assets/image-2.jpg'}
+            alt={post.frontmatter.title + '- Featured Shot'}
+          />
         )}
       </Link>
+
       <header>
         <h2 className='post-title'>
           <Link to={post.frontmatter.path} className='post-link'>
